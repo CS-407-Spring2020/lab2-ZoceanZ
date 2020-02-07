3ee85b53a8cb1fb38c950ef2efc10f3e409ffc23
Ocean <hzheng63@wisc.edu>
2020-02-07 14:40:56 -0600

    llll

--- app/src/main/java/com/example/lab2/MainActivity.java
+++ app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,10 +1,29 @@
 package com.example.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
 import android.os.Bundle;
-
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
 public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction (View view){
+        //Log.i("Info", "Nuclear Missle Launched");
+        EditText myTextField  = (EditText) findViewById(R.id.myTextField);
+        //Toast.makeText(MainActivity.this, myTextField.getText().toString(), Toast.LENGTH_LONG).show();
+        String str  = myTextField.getText().toString();
+        //System.out.print("xxxxxxx"+str);
+        goToActivity2(str);
+    }
+
+    public void goToActivity2(String s){
+        Intent intent = new Intent(this, Main2Activity.class);
+        intent.putExtra("m", s);
+        startActivity(intent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
